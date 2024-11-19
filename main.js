2a443bcaa0777cce7db7b55d93c861ffa44f8440
kasraalitabar <kasraalitabar@gmail.com>
2024-11-19 14:47:07 +0330

    done

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const box = document.querySelectorAll(".in")
+const box = document.querySelectorAll('.ina')
 
 function color(){
-    for(let i=0;i<box.length;i++){
-        const r =Math.floor(Math.random() *256)
-        const g =Math.floor(Math.random() *256)
-        const b =Math.floor(Math.random() *256)
+    for(let i=0; i < box.length; i++){
+        const r = Math.floor(Math.random() *256)
+        const g = Math.floor(Math.random() *256)
+        const b = Math.floor(Math.random() *256)
 
-        box[i].style.backgroundcolor = `rgb(${r},${g},${b})`
-    }
+        box[i].style.backgroundcolor = `rgb(${r}, ${g}, ${b})`
+    } 
 }
