e3e5989d56aec44e0b1135cf23727f3b75eb5f3c
kasraalitabar <kasraalitabar@gmail.com>
2024-11-19 14:28:00 +0330

    grid

---
+++ main.js
@@ -0,0 +1,11 @@
+const box = document.querySelectorAll(".in")
+
+function color(){
+    for(let i=0;i<box.length;i++){
+        const r =Math.floor(Math.random() *256)
+        const g =Math.floor(Math.random() *256)
+        const b =Math.floor(Math.random() *256)
+
+        box[i].style.backgroundcolor = `rgb(${r},${g},${b})`
+    }
+}
